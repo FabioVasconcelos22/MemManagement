31bd979a35637d4dfd803a1402da6a9502b5a4db
Fábio Vasconcelos <fabio.vasconcelos@fabamaq.com>
2020-03-30 16:37:09 +0100

    refactor

--- main.cpp
+++ main.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
 #include <memory>
 
-int GetFileLength (FILE *in ) {
+int GetFileSize (FILE *in ) {
     int length = 0;
 
     while (fgetc(in) != EOF) {
-        ++length;
+        length++;
     }
     return length;
 }
-
-void toUpper64bytes (FILE const& in, FILE & out, int pos) {
-    for (int i= 0; i<64; ++i) {
-        *(out+i) = toupper(*(in+i));
-    }
-};
 
 int main(int argc, char *argv[]) {
 
     FILE * inputFile;
     FILE * outputFile;
 
+    char buffer[64];
+
     inputFile = fopen ("../source.txt" , "r");
     outputFile = fopen ("../output.txt" , "w");
 
-    if(inputFile == nullptr || outputFile == nullptr) {
-        std::cerr << "Error opening the file" << std::endl;
+    if (inputFile == nullptr || outputFile == nullptr) {
+        std::cerr << "Error opening files" << std::endl;
         return 0;
     }
 
-    for ( int i = 0; i <= GetFileLength (inputFile); i+=64) {
-        toUpper64bytes(inputFile, outputFile, i);
+    if (fgetc(inputFile) == EOF) {
+        std::cerr << "Input file is empty" << std::endl;
+        return 0;
+    }
+
+    int size = GetFileSize(inputFile);
+
+    //return stream to the beggining
+    fseek(inputFile, 0, SEEK_SET);
+
+    while (fgetc(inputFile) != EOF) {
+
+        //come beck 1 position because of While verification
+        fseek(inputFile, -1, SEEK_CUR);
+
+        fgets(buffer, 64, inputFile);
+
+        for(int i=0 ; i< 64 ; ++i) {
+            buffer[i] = toupper(buffer[i]);
+            putc(buffer[i], outputFile);
+        }
     }
 
     fclose(inputFile);
