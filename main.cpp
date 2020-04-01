71a2afba16f9c174b1d16a49f01c069f94f3f000
Fábio Vasconcelos <fabio.vasconcelos@fabamaq.com>
2020-04-01 19:23:26 +0100

    MemoryPool

--- main.cpp
+++ main.cpp
@@ -1,55 +1,146 @@
 #include <iostream>
-#include <memory>
+#include <iomanip>
 
-int GetFileSize (FILE *in ) {
-    int length = 0;
+//class que recebe para metros tamanho dos blocos e qnts blocos ter
+        // metodo de alocador, parametro recebe tamanho) (retornar void*)
+        //free/ dialoc shunco de ram (*apontador)
 
-    while (fgetc(in) != EOF) {
-        length++;
+        //excptio se nao tiver memoria disponivel, se tamnho a alocar > bloco;
+
+
+        //major spoil: linked_list
+
+
+#include <malloc.h>
+
+class VirtualMemory {
+
+public:
+
+    uint8_t* p1;
+
+    int BlockQnt = 0;
+    int BlockSize = 0;
+
+private:
+
+    struct FreeBlock {
+        FreeBlock* next;
+    };
+
+    FreeBlock* Free;
+
+public:
+
+    VirtualMemory(int size, int qnt) :
+        BlockQnt (qnt),
+        BlockSize (size)
+    {
+        p1 = static_cast<uint8_t *>(malloc(qnt * size));
+
+        FreeBlock* block;
+        for (int i = 0 ; i < BlockQnt ; ++i) {
+            block  = (FreeBlock*) (p1 + BlockSize * i);
+            block->next = (FreeBlock*) (p1 + BlockSize * (i+1));
+        }
+
+        block->next = nullptr;
+
+        Free = (FreeBlock*) p1;
+
+        DebugMem();
+
+    };
+
+    ~VirtualMemory() {
+        std::cout << "Destructed" << std::endl;
     }
-    return length;
-}
+
+    void* AllocMem (int size) {
+
+        if (size > BlockSize) {
+            throw "TOO LARGE";
+        }
+
+        FreeBlock* aux = Free;
+
+        Free = Free->next;
+
+        if(Free->next == nullptr) {
+            throw "FULL_CAPACITY";
+        }
+
+        return aux;
+    }
+
+    void* DeallocMem(void* ptr) {
+
+        FreeBlock* aux = Free;
+
+        Free = (FreeBlock*)ptr;
+
+        Free->next = aux;
+
+        return Free;
+    }
+
+    void DebugMem () {
+
+        for(int i=0; i<BlockSize*BlockQnt; ++i) {
+            std::cout << " " << std::hex << std::setw(2) << std::setfill('0') << (int) p1[i];
+        }
+        std::cout << std::endl;
+    }
+private:
+
+};
 
 int main(int argc, char *argv[]) {
 
-    FILE * inputFile;
-    FILE * outputFile;
+    int* ptr;
+    int* ptr1;
 
-    char buffer[64];
+    VirtualMemory VM (16, 4);
 
-    inputFile = fopen ("../source.txt" , "r");
-    outputFile = fopen ("../output.txt" , "w");
+    ptr = (int*)VM.AllocMem(16);
+    ptr1 = ptr,
 
-    if (inputFile == nullptr || outputFile == nullptr) {
-        std::cerr << "Error opening files" << std::endl;
-        return 0;
-    }
+    *ptr = 0xFF;
 
-    if (fgetc(inputFile) == EOF) {
-        std::cerr << "Input file is empty" << std::endl;
-        return 0;
-    }
+    VM.DebugMem();
 
-    int size = GetFileSize(inputFile);
+    ptr = (int*)VM.AllocMem(16);
 
-    //return stream to the beggining
-    fseek(inputFile, 0, SEEK_SET);
+    *ptr = 0xBB;
 
-    while (fgetc(inputFile) != EOF) {
+    /**ptr = 0x0F;
+    *(ptr+1) = 0x0A;
+    *(ptr+2) = 0x0B;
+    *(ptr+3) = 0x01;
+    *(ptr+4) = 0x00;*/ //Recebenta a memória
 
-        //come beck 1 position because of While verification
-        fseek(inputFile, -1, SEEK_CUR);
+    VM.DebugMem();
 
-        fgets(buffer, 64, inputFile);
+    ptr = (int*)VM.DeallocMem(ptr);
 
-        for(int i=0 ; i< 64 ; ++i) {
-            buffer[i] = toupper(buffer[i]);
-            putc(buffer[i], outputFile);
-        }
-    }
+    VM.DebugMem();
 
-    fclose(inputFile);
-    fclose(outputFile);
+    ptr = (int*)VM.AllocMem(16);
+
+    *ptr = 0xAB;
+
+    VM.DebugMem();
+
+    ptr1 = (int*)VM.DeallocMem(ptr1);
+
+    VM.DebugMem();
+
+    ptr1 = (int*)VM.AllocMem(16);
+
+    *ptr1 = 0xAB;
+
+    VM.DebugMem();
+
 
     return 0;
 }
